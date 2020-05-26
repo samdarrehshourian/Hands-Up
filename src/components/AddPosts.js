d6806e16ab5b2ec9b766cffcf08284da81f272a4
Sam Darrehshourian <sam.zd@hotmail.se>
2020-05-26 15:00:19 +0200

    Created AddPosts component and changed the id #menu-bar to nav

---
+++ src/components/AddPosts.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../scss-style/AddPosts.scss'; 
+
+const  AddPosts = () => {
+  return (
+    <div className = 'add-post'>
+      <div id = 'add-post-title-container'>
+        <h4>Add Post</h4>
+      </div>
+        
+    </div>
+  );
+}
+
+export default AddPosts;

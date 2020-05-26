87c818524730bf0752908d235f04bf10f90d3921
Sam Darrehshourian <sam.zd@hotmail.se>
2020-05-26 11:08:20 +0200

    Changed the filename TopBar.js to Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../scss-style/Header.scss'; 
+
+const  Header = () => {
+  return (
+    <div className = 'top-bar'>
+        <h1>Header</h1>
+    </div>
+  );
+}
+
+export default Header;

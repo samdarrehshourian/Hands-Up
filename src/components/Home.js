645be7f9a667c906a440802c010fc5ade6ee65e6
Sam Darrehshourian <sam.zd@hotmail.se>
2020-05-26 16:31:05 +0200

    Made the clicked component show up in the middle

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,13 +4,9 @@ import AddPosts from './AddPosts.js'
 
 const  Home = () => {
   return (
-<<<<<<< HEAD
     <div className = 'home'>
         <h1>Home</h1>
         <AddPosts/>
-=======
-    <div className = 'Home'>
->>>>>>> c38e13bf4b030171c9723bc35d82c6d3aaaeb8f2
     </div>
   );
 }
--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -9,44 +9,46 @@ import '../scss-style/MenuBar.scss';
 
 const  MenuBar = () => {
   return (
-    <div id = 'menu-bar'>
+    <div className = 'container'>
         <Router>
-        <nav>
-            <ul>
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/courses">Courses</Link>
-                </li>
-                <li>
-                    <Link to="/messages">Messages</Link>
-                </li>
-                <li>
-                    <Link to="/achievements">Achievements</Link>
-                </li>
-                <li>
-                    <Link to="/profile">Profile</Link>
-                </li>
-            </ul>
-        </nav>
-        <Switch>
-            <Route exact path="/">
-                <Home/>
-            </Route>
-            <Route exact path="/courses">
-                <Courses/>
-            </Route>
-            <Route exact path="/messages">
-                <Messages/>
-            </Route>
-            <Route exact path="/achievements">
-                <Achievements/>
-            </Route>
-            <Route exact path="/profile">
-                <Profile/>
-            </Route>
-        </Switch>
+            <div id = 'menu-bar'>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/courses">Courses</Link>
+                        </li>
+                        <li>
+                            <Link to="/messages">Messages</Link>
+                        </li>
+                        <li>
+                            <Link to="/achievements">Achievements</Link>
+                        </li>
+                        <li>
+                            <Link to="/profile">Profile</Link>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+            <Switch>
+                <Route exact path="/">
+                    <Home/>
+                </Route>
+                <Route exact path="/courses">
+                    <Courses/>
+                </Route>
+                <Route exact path="/messages">
+                    <Messages/>
+                </Route>
+                <Route exact path="/achievements">
+                    <Achievements/>
+                </Route>
+                <Route exact path="/profile">
+                    <Profile/>
+                </Route>
+            </Switch>
         </Router>
     </div>
   );

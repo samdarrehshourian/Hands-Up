e391ae6c46538bfe316f5a061aea11c6fd306511
Sam Darrehshourian <sam.zd@hotmail.se>
2020-05-27 13:15:30 +0200

    Trying to push working code

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,11 +4,13 @@ import logo from '../icons/Logga+Header/handsUp_white.png'
 
 const  Header = () => {
   return (
-    <div id = 'Header'>
-      <img src={logo} alt="Logotype" width="30" height="30"></img>
+    <div id = 'Header-conatiner'>
+      <div id='Header'>
+        <img src={logo} alt="Logotype" width="30" height="30"></img>
         <p id="logo">Hands<b>Up</b></p>
         <p id ="user">Hi, Daniel</p>
         <div id="profilePicture"></div>
+      </div>
     </div>
   );
 }

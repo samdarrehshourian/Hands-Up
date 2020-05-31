08b8054b37d4f042d821b498ac6fab7223d027bb
Sam Zadeh Darrehshourian <sam.zd@hotmail.se>
2020-05-31 02:57:12 +0200

    Created a flex container and made the position fixed.

--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -3,9 +3,10 @@ import '../scss-style/Schedule.scss';
 
 const Schedule = () => {
   return (
-    <div id = 'schedule'>
+    <div id = 'schedule-container'>
+      <div className = 'schedule'>
         <h1>Schema</h1>
-        
+      </div>        
     </div>
   );
 }

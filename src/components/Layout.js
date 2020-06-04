605cd847e2ba7c4cd73e416ebfccac7f3ec78390
Daniel Villacorta <jdaniel.villacort@gmail.com>
2020-06-04 12:39:14 +0200

    Icons on the menu is fixed and also notification badge

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -7,6 +7,15 @@ import Achievements from './Achievements.js';
 import Profile from './Profile.js';
 import Schedule from './Schedule';
 import '../scss-style/Layout.scss'; 
+import homeLogo from '../icons/Sidebar_meny/Home.png'
+import coursesLogo from '../icons/Sidebar_meny/Corses.png'
+import achivmentsLogo from '../icons/Sidebar_meny/Achivments.png'
+import messageLogo from '../icons/Sidebar_meny/Message.png'
+import profileLogo from '../icons/Sidebar_meny/Profile.png'
+
+
+
+
 
 const  MenuBar = () => {
   return (
@@ -15,18 +24,24 @@ const  MenuBar = () => {
             <nav id = 'side-menu'>
                 <ul>
                     <li>
+                        <img src={homeLogo} alt="logo" />   
                         <Link to="/">Home</Link>
                     </li>
                     <li>
+                        <img src={coursesLogo} alt="logo" />
                         <Link to="/courses">Courses</Link>
                     </li>
                     <li>
-                        <Link to="/messages">Messages</Link>
-                    </li>
-                    <li>
+                        <img src={achivmentsLogo} alt="logo" />
                         <Link to="/achievements">Achievements</Link>
                     </li>
                     <li>
+                        <img src={messageLogo} alt="logo" />
+                        <Link to="/messages">Messages </Link>
+                        <span class="badge">2</span>
+                    </li>
+                    <li>
+                        <img src={profileLogo} alt="logo" />
                         <Link to="/profile">Profile</Link>
                     </li>
                 </ul>

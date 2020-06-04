62ebc389e2dbe2dbf72c056f64e9889a0d5eae85
Sam Zadeh Darrehshourian <sam.zd@hotmail.se>
2020-06-04 17:37:51 +0200

    Created posts

--- src/components/AddPosts.js
+++ src/components/AddPosts.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import profileImg from '../example-picture/example-picture.jpg';
 import chooseCourseIcon from '../icons/Flow/chooseCourse.png';
 import photoIcon from '../icons/Flow/photo.png';
 import tagFriendsIcon from '../icons/Flow/tagFriends.png';
+import Post from './Post.js' 
 import '../scss-style/AddPosts.scss'; 
 
 const  AddPosts = () => {
@@ -27,7 +28,7 @@ const  AddPosts = () => {
     {
       userImg: profileImg, 
       userName: "Frida Ylitalo", 
-      content: "Can someone share theire notes with me from todays lecture?", 
+      content: "Can someone share their notes with me from todays lecture?", 
       course: "Statistics",
       img: profileImg,
       id: 2
@@ -50,37 +51,58 @@ const  AddPosts = () => {
     }
   ]; 
 
+  const [postList, setPostList] = useState(posts);
+  const [newPost, setNewPost] = useState({});
+
+  const addPost = (event) => {
+    const newPostList = [...postList, newPost]; 
+    setPostList(newPostList); 
+    console.log(newPostList); 
+    event.preventDefault(); 
+}
+
+const handleChange = (event) => {
+  setNewPost({...newPost, [event.target.name]:event.target.value});
+  console.log({[event.target.name]:event.target.value}); 
+ 
+}
+
   return (
-    <div className = 'add-post-container'>
-      <div id = 'add-post-title'>
-        <h4>Add Post</h4>
-      </div>
-      <div className = 'add-post-content'>
-        <div id = 'pic-input-container'>
-          <img id = 'profile-pic' src={profileImg} alt='profile'/>
-          <input type='text' placeholder="Hi Daniel, what's on your mind?"/>
+    <div>
+      <div className = 'add-post-container'>
+        <div id = 'add-post-title'>
+          <h4>Add Post</h4>
         </div>
-        <div className = 'feature-container'>
-          <div className = 'feature'>
-            <img className = 'add-posts-icons' src={chooseCourseIcon} alt='Icon for choosing courses'/>
-            <select id = "course-list">
-              <option defaultValue value = "0">Choose course</option>
-              <option value = "1">Technology for Social Media</option>
-              <option value = "2">Discrete Mathematics</option>
-              <option value = "3">Javascript for beginners</option>
-              <option value = "4">Statistics</option>
-            </select>
+        <div className = 'add-post-content'>
+          <div id = 'pic-input-container'>
+            <img id = 'profile-pic' src={profileImg} alt='profile'/>
+            <input type='text' name="content" placeholder="Hi Daniel, what's on your mind?" onSubmit={addPost} onChange={handleChange}/>
           </div>
-          <div className = 'feature'>
-            <img className = 'add-posts-icons' src={photoIcon} alt='Icon for photos and videos'/>
-            <label className = 'feature-lable'>Photo/Video</label>
-          </div>
-          <div className = 'feature'>
-            <img className = 'add-posts-icons' src={tagFriendsIcon} alt='Icon for taging friends'/>
-            <label className = 'feature-lable'>Tag Friends</label>
+          <div className = 'feature-container'>
+            <div className = 'feature'>
+              <img className = 'add-posts-icons' src={chooseCourseIcon} alt='Icon for choosing courses'/>
+              <select id = "course-list">
+                <option defaultValue value = "0">Choose course</option>
+                <option value = "1">Technology for Social Media</option>
+                <option value = "2">Discrete Mathematics</option>
+                <option value = "3">Javascript for beginners</option>
+                <option value = "4">Statistics</option>
+              </select>
+            </div>
+            <div className = 'feature'>
+              <img className = 'add-posts-icons' src={photoIcon} alt='Icon for photos and videos'/>
+              <label className = 'feature-lable'>Photo/Video</label>
+            </div>
+            <div className = 'feature'>
+              <img className = 'add-posts-icons' src={tagFriendsIcon} alt='Icon for taging friends'/>
+              <label className = 'feature-lable'>Tag Friends</label>
+            </div>
           </div>
         </div>
       </div>
+      {posts.map(post => (
+                <Post key={post.id} post={post}/>
+            ))}
     </div>
   );
 }
--- src/components/Post.js
+++ src/components/Post.js
@@ -5,33 +5,33 @@ import like from '../icons/Flow/like.png';
 import share from '../icons/Flow/shareWithGroup.png';
 import '../scss-style/Post.scss'; 
 
-const  Post = () => {
-
-  return (
-      <div className = 'post-container'>
-        <div className = 'name-course-info-container'>
-            <img className = 'profile-pic' src={profileImg} alt='Profile'/>
-            <p>Daniel Villacorta</p>
-            <img className = 'arrow-icon' src={arrow} alt='Arrow-icon'/>
-            <p>Technology for Social Media</p>
+const  Post = (props) => {
+    console.log(props);
+    return (
+        <div className = 'post-container'>
+            <div className = 'name-course-info-container'>
+                <img className = 'profile-pic' src={props.post.userImg} alt='Profile'/>
+                <p>{props.post.userName}</p>
+                <img className = 'arrow-icon' src={arrow} alt='Arrow-icon'/>
+                <p>{props.post.course}</p>
+            </div>
+            <div className = 'content-container'>
+                <p className = 'content'>{props.post.content}</p>
+            </div>
+            <hr></hr>
+            <div className = 'like-share-container'>
+                <img className = 'icon' src={like} alt='Like-icon'/>
+                <p className = 'text' id = 'like-text'>Like</p>
+                <img className = 'icon' id = 'share-icon'src={share} alt='Share-icon'/>
+                <p className = 'text'>Share with group</p>
+            </div>
+            <hr></hr>
+            <div className = 'comment-container'>
+                <img className = 'comment-pic' src={profileImg} alt='Profile'/>
+                <input className = 'comment-input' type='text' placeholder="Add a comment ..."/>
+            </div>
         </div>
-        <div className = 'content-container'>
-            <p className = 'content'>Hello World!</p>
-        </div>
-        <hr></hr>
-        <div className = 'like-share-container'>
-            <img className = 'icon' src={like} alt='Like-icon'/>
-            <p className = 'text' id = 'like-text'>Like</p>
-            <img className = 'icon' id = 'share-icon'src={share} alt='Share-icon'/>
-            <p className = 'text'>Share with group</p>
-        </div>
-        <hr></hr>
-        <div className = 'comment-container'>
-            <img className = 'comment-pic' src={profileImg} alt='Profile'/>
-            <input className = 'comment-input' type='text' placeholder="Add a comment ..."/>
-        </div>
-      </div>
-  )
+    )
 }
 
 export default Post;
